9217a48aef656b2b237587c9c95d78685683c379
crazy <122232981+UBG10001@users.noreply.github.com>
2025-02-27 11:15:57 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Proxy all requests to open.spotify.com
+app.use('/', createProxyMiddleware({
+  target: 'https://gamejolt.net/?token=L6379njKysH7umSVzcWoLVpW4xABNG', // The target website to proxy
+  changeOrigin: true,
+  ws: true, // Support WebSocket connections
+  pathRewrite: {
+    '^/': '/', // Rewrite all incoming requests to open.spotify.com
+  },
+}));
+
+app.listen(port, () => {
+  console.log(`Proxy server running on http://localhost:${port}`);
+});
